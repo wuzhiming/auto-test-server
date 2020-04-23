c5c8eda42378b7a7e0a6b655fb14156b17b629a2
wuzhiming <wuzhiming.chukong@gmail.com>
2020-04-23 16:56:54 +0800

    add command server handler

---
+++ command.js
@@ -0,0 +1,45 @@
+let {spawn, exec} = require('child_process');
+const http = require('http');
+var url = require("url");
+var querystring = require("querystring");
+const PORT = 6788;
+const requestListener = async function (req, res) {
+    try {
+        res.writeHead(200);
+        //获取返回的url对象的query属性值
+        var arg = url.parse(req.url).query;
+
+        //将arg参数字符串反序列化为一个对象
+        var params = querystring.parse(arg);
+        if (!params.command) {
+            res.end('command not found');
+            return;
+        }
+        await execCommand(params.command);
+        res.end('success');
+    } catch (e) {
+        res.end('fail', e);
+    }
+};
+
+async function execCommand(command) {
+    if (!command) return Promise.reject();
+
+    console.log('exec command ', command);
+    return new Promise((resovle, reject) => {
+        let result = exec(command,
+            (err, stdout, stderr) => {
+                if (err) {
+                    console.error('exec err');
+                    reject();
+                }
+                stdout && console.log('stdout output--->', stdout.toString());
+                stderr && console.log('stderr output--->', stderr.toString());
+                resovle(stdout.toString());
+            });
+    })
+}
+
+const server = http.createServer(requestListener);
+server.listen(PORT);
+console.log('server start on port', PORT);
